b373213265dad6a5488f590777a8888ff24bffea
John Michael Kovachi <johnm.kovachi@gmail.com>
2018-03-11 01:00:04 -0500

    Add basic AR

--- App.js
+++ App.js
@@ -1,13 +1,45 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as THREE from 'three';
+import ExpoTHREE from 'expo-three';
+import Expo from 'expo';
+console.disableYellowBox = true;
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Expo.GLView
+        ref={(ref) => this._glView = ref}
+        style={{ flex: 1 }}
+        onContextCreate={this._onGLContextCreate}
+      />
     );
+  }
+
+  _onGLContextCreate = async (gl) => {
+    const arSession = await this._glView.startARSessionAsync();
+    const scene = new THREE.Scene();
+    const camera = ExpoTHREE.createARCamera(
+      arSession,
+      gl.drawingBufferWidth,
+      gl.drawingBufferHeight,
+      0.01,
+      1000
+    );
+    const renderer = ExpoTHREE.createRenderer({ gl });
+    renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
+    scene.background = ExpoTHREE.createARBackgroundTexture(arSession, renderer);
+    const geometry = new THREE.BoxGeometry(.07, .07, .07);
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+    cube.position.z = -0.4;
+    scene.add(cube);
+    const animate = () => {
+      requestAnimationFrame(animate);
+      renderer.render(scene, camera);
+      gl.endFrameEXP();
+    }
+    animate();
   }
 }
 

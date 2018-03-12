c834fee374d326de6200f5b2cc24cb0eb6702081
John Michael Kovachi <johnm.kovachi@gmail.com>
2018-03-11 21:58:51 -0400

    Add arrow helper, add custom font

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import * as THREE from 'three';
 import ExpoTHREE from 'expo-three';
 import Expo from 'expo';
 import Home from './src/Components/Home.js';
+import ColladaLoader from 'three-collada-loader';
 console.disableYellowBox = true;
 
 export default class App extends React.Component {
@@ -33,6 +34,11 @@ export default class App extends React.Component {
     }
   }
 
+  addArrow() {
+
+   
+  }
+
   _onGLContextCreate = async (gl) => {
     const arSession = await this._glView.startARSessionAsync();
     const scene = new THREE.Scene();
@@ -51,6 +57,59 @@ export default class App extends React.Component {
     const cube = new THREE.Mesh(geometry, material);
     cube.position.z = -0.4;
     scene.add(cube);
+
+    /*var geo = new THREE.Geometry();
+
+    geo.vertices.push(
+      new THREE.Vector3( -10,  10, 0 ),
+      new THREE.Vector3( -10, -10, 0 ),
+      new THREE.Vector3(  10, -10, 0 )
+    );
+
+    geo.faces.push( new THREE.Face3( 0, 1, 2 ) );
+
+    geo.computeBoundingSphere();
+
+    var mater = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+    var mesh = new THREE.Mesh( geo, mater );
+
+    mesh.position.z = -0.7;*/
+
+    var dir = new THREE.Vector3( 0, 0, -5 );
+
+    //normalize the direction vector (convert to vector of length 1)
+    dir.normalize();
+
+    var origin = new THREE.Vector3( 0, 0, -.3 );
+    var length = 1;
+    var hex = 0xffff00;
+
+    var arrowHelper = new THREE.ArrowHelper( dir, origin, length, hex );
+    arrowHelper.line.material.linewidth = 15;
+    scene.add( arrowHelper );
+
+    //scene.add( mesh );
+
+
+    /*const modelFiles = {
+      "model.dae": require('/home/jmkovachi/Documents/personal_projects/Clairvoyance/models/model.dae'),
+    }
+    const file = modelFiles["model.dae"];
+    const onProgress = xhr => {
+        if (xhr.lengthComputable) {
+            const percentComplete = xhr.loaded / xhr.total * 100;
+            console.log(Math.round(percentComplete, 2) + '% downloaded');
+        }
+    };
+    const assetProvider = assetNamed => {
+        return modelFiles[assetNamed];
+    };
+
+    const mesh = await loadBasicDAE();
+    scaleLongestSideToSize(mesh, 3);
+    alignMesh(mesh, { y: 1 });
+    scene.add(mesh);;*/
+
     const animate = () => {
       requestAnimationFrame(animate);
       renderer.render(scene, camera);
@@ -59,6 +118,25 @@ export default class App extends React.Component {
     animate();
   }
 }
+
+const onProgress = function(xhr) {
+  if (xhr.lengthComputable) {
+    const percentComplete = xhr.loaded / xhr.total * 100;
+    console.log(Math.round(percentComplete, 2) + '% downloaded');
+  }
+};
+
+
+async function loadBasicDAE() {
+  const model = {
+    "model.dae": require('/home/jmkovachi/Documents/personal_projects/Clairvoyance/models/model.dae'),
+  };
+
+  const collada = await ExpoTHREE.loadAsync([model['model.dae']], onProgress, name => model[name]);
+
+  return collada;
+}
+
 
 const styles = StyleSheet.create({
   container: {
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,23 +1,49 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, ImageBackground } from 'react-native';
 import * as THREE from 'three';
 import ExpoTHREE from 'expo-three';
 import Expo from 'expo';
+import { Font } from 'expo';
 console.disableYellowBox = true;
 
 export default class Home extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            fontLoaded: false,
+        };
+    }
+
+    async componentDidMount() {
+        await Font.loadAsync({
+          'Cinzel-Regular': require('/home/jmkovachi/Documents/personal_projects/Clairvoyance/assets/fonts/Cinzel-Regular.ttf'),
+        });
+
+        this.setState({
+            fontLoaded : true,
+        });
     }
 
     render() {
+
+        const resizeMode = 'center';
+
         return (
-            <View style={styles.flex}>
-                <Button
-                    onPress={this.props.handle}
-                    title={'Press me'}
-                />
-            </View>
+            
+            <ImageBackground style={styles.backgroundImage} source={require('/home/jmkovachi/Documents/personal_projects/Clairvoyance/assets/home.png')}>
+                <View style={styles.flex}>
+                    {
+                        this.state.fontLoaded ? (
+                            <Text style={styles.customFont}> Some Text </Text>
+                        ) : null
+                    }
+                    <Button
+                        onPress={this.props.handle}
+                        title={'Press me'}
+                    />
+                </View>
+            </ImageBackground>
+            
         );
     }
 
@@ -32,4 +58,13 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
     },
+    backgroundImage: {
+        flex: 1,
+        width: null,
+        height: null,
+        resizeMode: 'cover'
+    },
+    customFont: {
+        fontFamily: 'Cinzel-Regular'
+    }
   });
